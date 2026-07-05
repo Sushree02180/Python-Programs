cefbbdfdec66b27639fe8ebd42a189f37be628e5
Sushree Samikshya Dash <sushreesamikshyadash@gmail.com>
2026-07-05 11:12:14 +0530

    Add variables and data types demonstration
    # Variables and Data Types
    Description:-
    This project demonstrates how to declare variables and use different built-in data types in Python. It introduces strings, integers, floating-point numbers, and boolean values.
    
    Concepts Used:-
    - Variables
    - String (`str`)
    - Integer (`int`)
    - Float (`float`)
    - Boolean (`bool`)
    - print()
    
    Sample Output:-
    ----- Student Details -----
    Name: Sushree
    Age: 18
    Height: 5.4
    Student: True

---
+++ variables_datatypes.py
@@ -0,0 +1,14 @@
+# Program: Variables and Data Types in Python
+
+# Taking inputs
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (in feet): "))
+is_student = input("Are you a student? (True/False): ")
+
+# Displaying the values
+print("\n----- Student Details -----")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
