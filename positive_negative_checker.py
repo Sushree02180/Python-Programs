3031d4deb09c739d7c89efdab3ce639895013492
Sushree Samikshya Dash <sushreesamikshyadash@gmail.com>
2026-07-10 20:59:42 +0530

    Add positive, negative, or zero checker
    # Positive Negative Checker
    ## Description
    This project demonstrates how to determine whether a number entered by the user is positive, negative, or zero using Python's conditional statements.
    
    ## Concepts Used
    - `input()`
    - `int()`
    - Variables
    - Comparison Operators (`>`, `<`)
    - `if-elif-else`
    - `print()`
    
    ## Sample Output
    Example 1
    Enter a number: 25
    The number is Positive.
    Enter a number: -8
    The number is Negative.
    Enter a number: 0
    The number is Zero.

---
+++ positive_negative_checker.py
@@ -0,0 +1,10 @@
+# Positive, Negative, or Zero Checker
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print(number, "is a Positive number.")
+elif number < 0:
+    print(number, "is a Negative number.")
+else:
+    print("The entered number is Zero.")
