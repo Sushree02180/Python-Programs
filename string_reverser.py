79164f1b828d6a2d4dee1cb5641de3dcab8640fd
Sushree Samikshya Dash <sushreesamikshyadash@gmail.com>
2026-07-11 19:49:19 +0530

    Add String Reverser using string slicing
    # String Reverser
    ## Description
    This project demonstrates how to reverse a string entered by the user using a user-defined function in Python.
    
    ## Concepts Used
    - `input()`
    - Functions
    - `return`
    - Strings
    - String Slicing
    - Variables
    - `print()`
    
    ## Sample Output
    Enter a string: Python
    Reversed string: nohtyP
    Enter a string: Bioinformatics
    Reversed string: scitamrofnioiB

---
+++ string_reverser.py
@@ -0,0 +1,14 @@
+# String Reverser
+
+# Function to reverse a string
+def reverse_string(text):
+    return text[::-1]
+
+# Taking input from the user
+text = input("Enter a string: ")
+
+# Calling the function
+reversed_text = reverse_string(text)
+
+# Displaying the result
+print("Reversed string:", reversed_text)
