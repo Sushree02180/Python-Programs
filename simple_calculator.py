8a630d8dc0166b0ee5389f5dced384c81966bb64
Sushree Samikshya Dash <sushreesamikshyadash@gmail.com>
2026-07-11 13:18:11 +0530

    Add Calculator using user-defined functions
    # Calculator using Functions
    ## Description
    This project demonstrates how to perform basic arithmetic operations using user-defined functions in Python.
    
    ## Concepts Used
    - `input()`
    - `float()`
    - Functions
    - `return`
    - Parameters
    - Arithmetic Operators
    - `print()`
    
    ## Sample Output
    Enter the first number: 20
    Enter the second number: 5
    Addition: 25.0
    Subtraction: 15.0
    Multiplication: 100.0
    Division: 4.0

---
+++ simple_calculator.py
@@ -0,0 +1,27 @@
+ # Calculator using Functions
+
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+
+# Function to multiply two numbers
+def multiply(a, b):
+    return a * b
+
+# Function to divide two numbers
+def divide(a, b):
+    return a / b
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Displaying the results
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
